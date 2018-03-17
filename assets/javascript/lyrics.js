3171841b122af8a746af20a6cc9b6ca72635477a
Shelby Smith <shelbysmith@Shelbys-MacBook-Pro.local>
2018-03-17 09:54:35 -0500

    adding lyrics js

---
+++ assets/javascript/lyrics.js
@@ -0,0 +1,27 @@
+$("#song-search-btn").on("click", function(event){
+    event.preventDefault();
+    var artistSearch = $("#artist-search").val();
+    var titleSearch = $("#title-search").val();
+
+
+
+      var queryURL = "https://orion.apiseeds.com/api/music/lyric/" + artistSearch + "/" + titleSearch + "?apikey=0h9uFKahJatBRHJodKUdrmqLEqp360ySpHaaFMeAgVoi4jPViLLQaYTIhdGyPjQm";
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+
+        //for loop or replace for the entire string to make them new lines
+        var fix = response.result.track.text;
+        while (fix.indexOf("\n") !== -1) {
+          fix = fix.replace("\n", "<br>")
+
+        }
+
+        $("#lyrics-display").html(fix);
+        console.log(response.result.track.text.indexOf("\n"))
+
+      });
+    });
