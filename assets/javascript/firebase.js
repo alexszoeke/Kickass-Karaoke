57df144f1e439d9be4fd1d2125928f506a9410be
Antony Laroche <antony@larochefamily.net>
2018-03-16 23:55:49 -0500

    Completed sorted list of searches by frequency and appending DOM

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -20,7 +20,13 @@ $(document).ready(function () {
   var artistSearch = "";
   var songSearch = "";
   var userID = "";
-  var userReference="";
+  var userReference = "";
+  var globalSearchArtist;
+  var globalSearchSong;
+  var arrayGlobeSearchArtist = [];
+  var arrayGlobeSearchSong = [];
+  var globeArtist = {};
+  var globeSong = {};
 
   $("#register").on("click", function (event) {
     // Creating a new user
@@ -56,22 +62,31 @@ $(document).ready(function () {
   //Test if there is a user signed in
   auth.onAuthStateChanged(function (user) {
     if (user) {
-      userID =user.email;
-      userReference=user.uid;
+      userID = user.email;
+      userReference = user.uid;
+      $("#register").hide();
+      // $("#login").hide();
+      $("#logout").show();
       console.log(user)
       console.log(user.email);
     } else {
       console.log("not logged in");
+      $("#register").show();
+      $("#login").show();
+      $("#logout").hide();
     }
   });
 
   //TODO: Add sign out button to Sign out a user
-  auth.signOut().then(function () {
-    // Sign-out successful.
-  }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode, errorMessage);
+  $("#logout").on("click", function () {
+    event.preventDefault();
+    auth.signOut().then(function () {
+      console.log("signout successful");
+    }).catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
   });
 
   //saving searches to database
@@ -92,5 +107,66 @@ $(document).ready(function () {
     }
   })
 
+  // Database listener for searches if user not signed in
+
+  refDatabase.ref("/searches/artist").on("value", function (snapshot) {
+
+    arrayGlobeSearchArtist = snapshotToArray(snapshot);
+
+    globeArtist = countGlobalArtistSearch(arrayGlobeSearchArtist);
+
+    //Sort by number of times searched
+    var sortedGlobeArtist = Object.keys(globeArtist).sort(function (a, b) {
+      return globeArtist[b] - globeArtist[a]
+    })
+    //TODO:  build for loop to use sorted artist array instead of object
+    Object.keys(globeArtist).forEach(function (key) {
+      var text = $("<p></p>").text(key)
+      $("#recent-display").append(text);
+    });
+
+    console.log(globeArtist);
+
+    console.log(sortedGlobeArtist);
+
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  })
+
+  refDatabase.ref("/searches/song").on("value", function (snapshot) {
+
+    arrayGlobeSearchSong = snapshotToArray(snapshot);
+    globeSong = countGlobalArtistSearch(arrayGlobeSearchSong);
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  })
+
+
+
+  // Append list of artist to Recent Sear
+
+
+  function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function (childSnapshot) {
+      var item = childSnapshot.val();
+      item.key = childSnapshot.key;
+
+      returnArr.push(item);
+    });
+
+    return returnArr;
+  };
+
+  function countGlobalArtistSearch(arr) {
+
+    var count = {};
+    arr.forEach(function (i) {
+      count[i] = (count[i] || 0) + 1;
+    });
+    console.log(count);
+    return count;
+  }
 
 })
